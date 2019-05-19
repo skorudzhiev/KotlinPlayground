ae6f9771d247b937a7af989f0d67465e9c092578
skorudzhiev <skorudzhiev@gmail.com>
2019-05-19 10:29:16 +0300

    Greetings Kotlin
    Using functions to print a greeting message based on current day of the
    week and time of the day
    * Using nested functions in a single file
    * Calendar methods provided by java.util library
    * Decision statements logic - When, and If statement

--- app/src/main/java/com/example/kotlinplayground/AMS.kt
+++ app/src/main/java/com/example/kotlinplayground/AMS.kt
@@ -1,5 +1,43 @@
 package com.example.kotlinplayground
 
-fun main(){
-    println("Hello World")
+import java.util.*
+
+val day = currentDayUtil()
+
+fun main() {
+    println("Hello World!")
+
+    dayOfWeek()
+    greetingAmOrPm()
+}
+
+fun dayOfWeek() {
+    println("What day is it today?")
+    currentDayPrint()
+}
+
+fun currentDayPrint() {
+    println( when (day) {
+            1 -> "Sunday"
+            2 -> "Monday"
+            3 -> "Tuesday"
+            4 -> "Wednesday"
+            5 -> "Thursday"
+            6 -> "Friday"
+            7 -> "Saturday"
+            else -> "Time has stopped"
+        }
+    )
+}
+
+fun greetingAmOrPm() {
+    println(if (currentTimeUtil() < 12) "Good Morning" else "Good Night")
+}
+
+fun currentDayUtil(): Int {
+    return Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+}
+
+fun currentTimeUtil(): Int {
+    return Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
 }
