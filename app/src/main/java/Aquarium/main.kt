6885780ce5a02c2b0d2f6558403986345c2897d1
skorudzhiev <skorudzhiev@gmail.com>
2019-05-25 09:22:28 +0300

    Organizing in classes, methods, getters & setters
    Started to build the aquarium in an object oriented way.
     * Aquarium class was built to serve as a data class
     	- It contains variables and function with getter and setter
    	method
     * main class contains internal function to build the aquarium by
     accessing the methods from the data class and return output on the
     screen
     	- Visibility modifiers are introduced

---
+++ app/src/main/java/Aquarium/main.kt
@@ -0,0 +1,19 @@
+package Aquarium
+
+fun main() {
+    buildAquarium()
+}
+
+internal fun buildAquarium() {
+    val myAquarium = Aquarium()
+
+    println("Length: ${myAquarium.length} " +
+            "Width: ${myAquarium.width} " +
+            "Height: ${myAquarium.height}")
+
+    myAquarium.height = 80
+
+    println("Height: ${myAquarium.height} cm")
+
+    println("Volume: ${myAquarium.volume} liters")
+}
