57699a2d823ea29320682fab8f25ff751e6c28b8
skorudzhiev <skorudzhiev@gmail.com>
2019-05-25 09:39:42 +0300

    Structuring spices into objects
    Creating a separate package folder with a classes for a SimpleSpice and
    displaying the spice itself
     * The spice have properties for name, spiciness and heat (which is an
     int) return with a getter property

---
+++ app/src/main/java/spices/SimpleSpice.kt
@@ -0,0 +1,8 @@
+package spices
+
+class SimpleSpice {
+    val name: String = "curry"
+    val spiciness: String = "mild"
+    val heat: Int
+    get() {return 5}
+}
---
+++ app/src/main/java/spices/Spice.kt
@@ -0,0 +1,6 @@
+package spices
+
+fun main() {
+    val simpleSpice = SimpleSpice()
+    println("${simpleSpice.name} ${simpleSpice.heat}")
+}
