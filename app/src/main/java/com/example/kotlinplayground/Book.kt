a3c3600a652d559898c61d860350ecda9b7e47be
skorudzhiev <skorudzhiev@gmail.com>
2019-06-22 15:48:29 +0300

    Introducing pairs and triples datatypes
    Pairs are neat Kotlin datatype that allows us to define a generic pair
    of values, which you could use to return more than one values from a function.
    
    This commit introduces an additional functionality to the Book class by:
    * Introducing additional property in the class for year
    * Creating a method that makes use of pairs to return both the title and the author
    * Creating a method that makes use of triples to return the title, author and year
    * Creating an instance of the class and its encompassing methods, and printing their values at runtime

--- app/src/main/java/com/example/kotlinplayground/Book.kt
+++ app/src/main/java/com/example/kotlinplayground/Book.kt
@@ -1,15 +1,35 @@
 package com.example.kotlinplayground
 
-open class Book(val title: String, val author: String) {
+fun main() {
+
+    val book = Book("Romeo and Juliet", "William Shakespeare", 1597)
+    val bookTitleAuthor = book.getTitleAuthor()
+    val bookTitleAuthorYear = book.getTitleAuthorYear()
+
+    println("Here is your book ${bookTitleAuthor.first} by ${bookTitleAuthor.second}")
+
+    println("Here is your book ${bookTitleAuthorYear.first} " +
+            "by ${bookTitleAuthorYear.second} written in ${bookTitleAuthorYear.third}")
+}
+
+open class Book(val title: String, val author: String, val year: Int) {
 
     private var currentPage = 1
 
     open fun readPage() {
         currentPage++
     }
+
+    fun getTitleAuthor(): Pair<String, String> {
+        return (title to author)
+    }
+
+    fun getTitleAuthorYear(): Triple<String, String, Int> {
+        return Triple(title, author, year)
+    }
 }
 
-class eBook(title: String, author: String, var format: String = "text") : Book(title, author) {
+class eBook(title: String, author: String, var format: String = "text") : Book(title, author, year = 1990) {
 
     private var wordsRead = 0
 
