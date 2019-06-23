9acc5b20951ae3fffec54e48bb0fa49f22f43da5
skorudzhiev <skorudzhiev@gmail.com>
2019-06-23 10:34:54 +0300

    Extension functions in Books exercise
    This commit introduces the following changes in Book.kt:
    * Adding a mutable property in Book class
    - This enforced to make the class open, instead of abstract
    * Creating an extension function of books to return the weight of a book
    based on the number of pages multipled by 1.5 grams
    * Creating an extension function tornPages(), that takes the number of
    pages as an argument and changes the page count
    * Introducing new class called "Puppy", giving the it the book to play
    with, leaves the book with no more pages
    - The class is initialized in the main function, where in a while
    function the puppy is tearing the book
    - The thread sleeps for 320 millis between the cycles to make the UX
    more entertaining

--- app/src/main/java/com/example/kotlinplayground/Book.kt
+++ app/src/main/java/com/example/kotlinplayground/Book.kt
@@ -1,10 +1,12 @@
 package com.example.kotlinplayground
 
+import java.util.*
+
 const val MAX_NUMBER_BOOKS = 20
 
 fun main() {
 
-    val book = Book("Romeo and Juliet", "William Shakespeare", 1597)
+    val book = Book("Romeo and Juliet", "William Shakespeare", 1597, 240)
     val bookTitleAuthor = book.getTitleAuthor()
     val bookTitleAuthorYear = book.getTitleAuthorYear()
 
@@ -23,9 +25,19 @@ fun main() {
     println(moreBooks)
 
     book.printUrl()
+
+    val puppy = Puppy()
+    val book2 = Book("Oliver Twist", "Charles Dickens", 1837, 540)
+
+    while (book2.pages > 0) {
+        puppy.playWithBook(book2)
+        Thread.sleep(320)
+        println("${book2.pages} left in ${book2.title}")
+    }
+    println("Sad puppy, no more pages in ${book2.title}. ")
 }
 
-open class Book(val title: String, val author: String, val year: Int) {
+open class Book(val title: String, val author: String, val year: Int, var pages: Int) {
 
     companion object {
             const val BASE_URL = "http://www.turtlecare.net/"
@@ -54,11 +66,21 @@ open class Book(val title: String, val author: String, val year: Int) {
     }
 }
 
-class eBook(title: String, author: String, var format: String = "text") : Book(title, author, year = 1990) {
+fun Book.weight() : Double { return (pages * 1.5) }
+
+fun Book.tornPages(torn: Int) = if (pages >= torn) pages -= torn else pages = 0
+
+class eBook(title: String, author: String, var format: String = "text") : Book(title, author, year = 1990, pages = 100) {
 
     private var wordsRead = 0
 
     override fun readPage() {
         wordsRead += 250
+    }
+}
+
+class Puppy {
+    fun playWithBook(book: Book) {
+        book.tornPages(Random().nextInt(12))
     }
 }
