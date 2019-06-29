84d900561f12c7471f8ec2f915b35347bfc6436f
skorudzhiev <skorudzhiev@gmail.com>
2019-06-29 15:59:21 +0300

    Practice higher-order functions by creating a Game
    This commit introduces the following changes in code:
    * New Game class with directions along with start and end point as a
    nested enum class
    * Inside Game was declared a var that is a mutable list initialized with
    the "start" element
    * Creating 4 lambdas:
    - north, south, east, west, that add the respective direction to the
    path
    - add another lambda, end, that:
    -- adds END to path
    -- prints the path
    -- clears the path
    -- returns false
    * main function was created to initialize the game and print the game
    values

---
+++ app/src/main/java/com/example/kotlinplayground/Game.kt
@@ -0,0 +1,25 @@
+package com.example.kotlinplayground
+
+fun main() {
+    val game = Game()
+    println(game.path)
+    game.north()
+    game.south()
+    game.east()
+    game.west()
+    game.end()
+    println(game.path)
+}
+
+enum class Direction {
+    NORTH, EAST, WEST, SOUTH, START, END
+}
+
+class Game {
+    var path = mutableListOf(Direction.START)
+    val north = { path.add(Direction.NORTH) }
+    val south = { path.add(Direction.SOUTH) }
+    val east = { path.add(Direction.EAST) }
+    val west = { path.add(Direction.WEST) }
+    val end = { path.add(Direction.END); println("Game Over: $path"); path.clear(); false }
+}
