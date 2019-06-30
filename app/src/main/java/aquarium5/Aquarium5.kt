0baa013bc3c641bacaa0bfc5edf619850c5e768d
skorudzhiev <skorudzhiev@gmail.com>
2019-06-30 12:49:44 +0300

    Introducing higher-order functions in new class
    Basic usage of these functions has been introduced in code along with
    extension lambdas:
    * with function
    * custom with function
    * run as an extension that works with all data types
    * apply as a similar extension to run
    * let as a representation of chaining manipulations together

--- app/src/main/java/aquarium5/Aquarium5.kt
+++ app/src/main/java/aquarium5/Aquarium5.kt
@@ -1,2 +1,30 @@
 package aquarium5
 
+data class Fish(var name: String)
+
+fun main() {
+    fishExamples()
+}
+
+fun fishExamples() {
+    val fish = Fish("splashy")
+
+    myWith(fish.name) {
+        println(capitalize())
+    }
+
+    println(fish.run { name })
+    println(fish.apply {})
+
+    val fish2 = Fish(name = "Splashy").apply { name = "Sharky" }
+    println(fish2)
+
+    println(fish.let { it.name.capitalize() }
+        .let { "$it fish" }
+        .let { it.length }
+        .let { it + 31 })
+}
+
+fun myWith(name: String, block: String.() -> Unit) {
+    name.block()
+}
