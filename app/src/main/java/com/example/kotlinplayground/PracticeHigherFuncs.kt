80b3a512fde77a9064286605a8a4fa8f9d8e62be
skorudzhiev <skorudzhiev@gmail.com>
2019-06-30 12:57:06 +0300

    Extension of list using a higher-order function
    The extension returns all the numbers in the list that are divisible by
    3

---
+++ app/src/main/java/com/example/kotlinplayground/PracticeHigherFuncs.kt
@@ -0,0 +1,19 @@
+package com.example.kotlinplayground
+
+fun main() {
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+    print(numbers.divisibleBy {
+        it.rem(3)
+    })
+}
+
+
+fun List<Int>.divisibleBy(block: (Int) -> Int): List<Int> {
+    val result = mutableListOf<Int>()
+    for (item in this) {
+        if (block(item) == 0) {
+            result.add(item)
+        }
+    }
+    return result
+}
